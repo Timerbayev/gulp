f98ec14f18f6283537e421063563c10598ebe1c8
Aidar <account_service@bk.ru>
2018-11-29 23:09:05 +0300

    gulp_1

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,25 @@
-const gulp = require("gulp");
-const del = require("del");
-const babel = require("gulp-babel");
-const concat = require("gulp-concat");
-const uglify = require("gulp-uglify");
-const clean = ()=>{del(["assets"])};
+const gulp = require("gulp")
+const babel = require("gulp-babel")
+const uglify = require("gulp-uglify")
+const concat = require("gulp-concat")
+const del  = require("del")
+let clean  = (cb)=>{del(["assets"]);cb()}
+
+
+
+function scripts() { return  gulp.src("src/**/*.js")
+    .pipe(babel({presets: ['@babel/env']}))
+    .pipe(uglify())
+    .pipe(concat("main.min.js"))
+    .pipe(gulp.dest("assets/scripts/"))
+
+}
+function jsons() {return  gulp.src("src/**/*.json")
+    .pipe(gulp.dest("assets/jsons/"))
+}
+
+gulp.task("default", gulp.series(clean, scripts, jsons))
+
 gulp.task("clean",(cb)=>{del(["assets"]);cb();});
 gulp.task("script",function () { return gulp.src("src/**/*.js")
     .pipe(babel({
@@ -12,9 +28,8 @@ gulp.task("script",function () { return gulp.src("src/**/*.js")
     .pipe(uglify())
     .pipe(concat("main.min.js"))
     .pipe(gulp.dest("assets/scripts/"))
-    
+
 });
 gulp.task("json",function () { return  gulp.src("src/**/*.json").pipe(gulp.dest("assets/jsons/"))
 
 });
-gulp.task("default", gulp.series("clean","script","json"));
